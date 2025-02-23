36025586a4ea36dc43f4a853d9ba81bbd05ca81d
psmyth2 <pat@2ndnaturewater.com>
2025-02-23 09:39:25 -0800

    update road/trail accessibility queries

--- app/reference_layers.py
+++ app/reference_layers.py
@@ -2,14 +2,14 @@ trails_roads = [
     {
         "name": "usfs_trails",
         "url": "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_TrailNFSPublish_01/MapServer/0",
-        "fields": ["TRAIL_NAME", "GIS_MILES"],
-        "query": '1=1'
+        "fields": ["TRAIL_NAME", "GIS_MILES", "ACCESSIBILITY_STATUS", "TERRA_MOTORIZED"],
+        "query": "ACCESSIBILITY_STATUS <> 'NOT ACCESSIBLE' AND TERRA_MOTORIZED <> 'N'"
     },
     {
         "name": "usfs_roads",
         "url": "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RoadBasic_01/MapServer/0",
-        "fields": ["NAME", "GIS_MILES"],
-        "query": '1=1'
+        "fields": ["NAME", "GIS_MILES", "OPENFORUSETO"],
+        "query": "OPENFORUSETO = 'ALL'"
     },
     {
         "name": "usfs_rec_sites",
@@ -25,7 +25,8 @@ reference_layers = [
         "url": "https://portal.opentopography.org/API/globaldem",
     },
     {
-        "name": "osm",
-        "url": "https://portal.opentopography.org/API/globaldem",
+        "name": "weather",
+        "url": "https://api.openweathermap.org/data/2.5/weather",
     }
+
 ]
