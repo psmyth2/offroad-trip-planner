096aed8990c1e1fef8ae283f03baa7a6a966a5fb
psmyth2 <pat@2ndnaturewater.com>
2025-02-23 17:11:57 -0800

    update queries for reference layers

--- app/reference_layers.py
+++ app/reference_layers.py
@@ -3,13 +3,13 @@ trails_roads = [
         "name": "usfs_trails",
         "url": "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_TrailNFSPublish_01/MapServer/0",
         "fields": ["TRAIL_NAME", "GIS_MILES", "ACCESSIBILITY_STATUS", "TERRA_MOTORIZED"],
-        "query": "ACCESSIBILITY_STATUS <> 'NOT ACCESSIBLE' AND TERRA_MOTORIZED <> 'N'"
+        "query": "TERRA_MOTORIZED <> 'N'"
     },
     {
         "name": "usfs_roads",
         "url": "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RoadBasic_01/MapServer/0",
         "fields": ["NAME", "GIS_MILES", "OPENFORUSETO"],
-        "query": "OPENFORUSETO = 'ALL'"
+        "query": "OPENFORUSETO = 'ALL' OR OPENFORUSETO = 'PUBLIC'"
     },
     {
         "name": "usfs_rec_sites",
